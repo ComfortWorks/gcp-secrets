31aa323e1bed953507fa427317c0461e122f72a1
shaafiee <shaafiee@gmail.com>
2023-10-03 09:31:13 +0800

    clean up

--- secrets.py
+++ secrets.py
@@ -1,7 +1,6 @@
 from google.cloud import secretmanager
 import os
 import requests
-#from settings import GCP_PROJECT_ID, currentSubdomain, user, pwd, branch
 
 
 def loadSecret(secret):
@@ -10,12 +9,8 @@ def loadSecret(secret):
 		if not GCP_PROJECT_ID:
 			GCP_PROJECT_ID = requests.get("http://metadata.google.internal/computeMetadata/v1/project/project-id")
 		name = f"projects/{GCP_PROJECT_ID}/secrets/{secret}/versions/latest"
-		print("project:" + name)
 		client = secretmanager.SecretManagerServiceClient()
 		account = client.access_secret_version(request={"name":name}).payload.data.decode("UTF-8")
-		#print(account)
-		#print(type(account))
-		#return client.access_secret_version(request={"name":name}).payload.data.decode("UTF-8")
 		return account
 	except Exception as e:
 		print(e)
@@ -24,19 +19,4 @@ def loadSecret(secret):
 
 def getSecret(secretName):
 	return loadSecret(secretName)
-
-"""
-def serviceAccount():
-	return loadSecret("serviceAccount")
-	
-def googleProject():
-	return loadSecret("googleProject")
-	
-def shopifyApiCred():
-	return loadSecret("shopifyApiCred")
-	
-def myshopifySubdomain():
-	return loadSecret("myshopifySubdomain")
-"""
-	
 
